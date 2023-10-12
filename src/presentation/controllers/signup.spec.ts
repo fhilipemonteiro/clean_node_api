114a4f4bca2a9672aa1ed8b5a086dac45b36e8cf
Fhilipe Monteiro <fhilipemonteiro@hotmail.com>
2023-10-12 01:32:34 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,4 +17,21 @@ describe('SignUp Controller', () => {
     expect(httpResponde.statusCode).toBe(400)
     expect(httpResponde.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Shold return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponde = sut.handle(httpRequest)
+
+    expect(httpResponde.statusCode).toBe(400)
+    expect(httpResponde.body).toEqual(new Error('Missing param: email'))
+  })
 })
